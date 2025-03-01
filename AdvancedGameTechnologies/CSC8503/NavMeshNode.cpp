8510b65dd624b492d37c0c498491c0df5a7667d5
kyledarrenanderson <91319534+kyledarrenanderson@users.noreply.github.com>
2025-03-01 03:08:50 +0000

    added logic to generate NavMeshGrid
    Needs to be changed to match logic in comments.
    
    Missing logic to determine if a slope is too steep

---
+++ AdvancedGameTechnologies/CSC8503/NavMeshNode.cpp
@@ -0,0 +1,10 @@
+#include "NavMeshNode.h"
+
+using namespace NCL;
+using namespace CSC8503;
+
+void NavMeshNode::checkObstructed() {
+	//logic to determine if a node is obstructed by an environment object
+
+	this->obstructed = false;
+}
