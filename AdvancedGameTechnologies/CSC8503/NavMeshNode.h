f6760fdfcf5fa8c19db520581ae22d44e6235cd1
kyledarrenanderson <91319534+kyledarrenanderson@users.noreply.github.com>
2025-03-01 03:08:50 +0000

    added logic to generate NavMeshGrid
    Needs to be changed to match logic in comments.
    
    Missing logic to determine if a slope is too steep

--- AdvancedGameTechnologies/CSC8503/NavMeshNode.h
+++ AdvancedGameTechnologies/CSC8503/NavMeshNode.h
@@ -17,13 +17,22 @@ namespace NCL {
 			NavMeshNode(NCL::Maths::Vector3 position) {
 				position = position;
 			}
+			void checkObstructed();
+			bool isObstructed() {
+				return obstructed;
+			}
+			bool detectImpassableEdge();
+			void calculateEdges();
+			void addEdge(NavMeshNode* neighbour, int cost) {
+				Edge e;
+				e.neighbour = neighbour;
+				e.cost = cost;
+				edges.push_back(e);
+			}
 		protected:
 			std::vector<Edge> edges;
 			NCL::Maths::Vector3 position;
 			bool obstructed;
-			bool checkObstructed();
-			bool detectImpassableEdge();
-			void calculateEdges();
 		};
 	}
 }
