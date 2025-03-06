cd07c86d818e01b7a471e7ceedba16d783ba686d
kyledarrenanderson <91319534+kyledarrenanderson@users.noreply.github.com>
2025-03-06 09:23:15 +0000

    setup header file for NavMeshAgent

--- AdvancedGameTechnologies/CSC8503/NavMeshAgent.h
+++ AdvancedGameTechnologies/CSC8503/NavMeshAgent.h
@@ -1,1 +1,26 @@
 #pragma once
+#include "NavMeshGrid.h"
+#include "NavMeshNode.h"
+#include <vector>
+
+class NavMeshGrid;
+class NavMeshNode;
+
+namespace NCL {
+	namespace CSC8503 {
+		class NavMeshAgent {
+		public:
+			std::vector<NavMeshNode> FindPath(NavMeshNode currentNode, NavMeshNode destination);
+			void clearPath();
+		protected:
+			float newFScore;
+			float newGScore;
+			float newHeuristic;
+			std::vector<NavMeshNode*> openList;
+			std::vector<NavMeshNode*> closedList;
+			std::vector<NavMeshNode*> path;
+			NavMeshNode* destination;
+			NavMeshNode* currentNode;
+		};
+	}
+}
