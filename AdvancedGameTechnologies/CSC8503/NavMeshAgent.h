e1381eee10648a655706712314cce068aaf34f00
kyledarrenanderson <91319534+kyledarrenanderson@users.noreply.github.com>
2025-03-06 09:48:16 +0000

    Wrote constructor, added new needed methods

--- AdvancedGameTechnologies/CSC8503/NavMeshAgent.h
+++ AdvancedGameTechnologies/CSC8503/NavMeshAgent.h
@@ -10,17 +10,32 @@ namespace NCL {
 	namespace CSC8503 {
 		class NavMeshAgent {
 		public:
-			std::vector<NavMeshNode> FindPath(NavMeshNode currentNode, NavMeshNode destination);
+			NavMeshAgent() {
+				this->newFScore = 0;
+				this->newGScore = 0;
+				this->heuristic = 0;
+				this->currentLevelGrid = NULL;
+				this->currentNode = NULL;
+				this->node = NULL;
+				this->destination = NULL;
+			}
+			std::vector<NavMeshNode> FindPath(NavMeshNode* currentNode, NavMeshNode* destination);
 			void clearPath();
+			float calculateHeuristic(NavMeshNode* currentNode, NavMeshNode* destination);
+			float calculateGScore(NavMeshNode* node);
+			float calculateFScore(NavMeshNode* node);
+			void setCurrentNode();
 		protected:
 			float newFScore;
 			float newGScore;
-			float newHeuristic;
+			float heuristic;
 			std::vector<NavMeshNode*> openList;
 			std::vector<NavMeshNode*> closedList;
 			std::vector<NavMeshNode*> path;
+			NavMeshGrid* currentLevelGrid;
 			NavMeshNode* destination;
 			NavMeshNode* currentNode;
+			NavMeshNode* node;
 		};
 	}
 }
