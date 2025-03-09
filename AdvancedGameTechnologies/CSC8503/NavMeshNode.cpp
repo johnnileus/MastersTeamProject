fae3bffca67f1644f623da61473939b4ca30facd
kyledarrenanderson <91319534+kyledarrenanderson@users.noreply.github.com>
2025-03-09 02:12:20 +0000

    Fixed functions calls to match renamed functions

--- AdvancedGameTechnologies/CSC8503/NavMeshNode.cpp
+++ AdvancedGameTechnologies/CSC8503/NavMeshNode.cpp
@@ -9,24 +9,24 @@ void NavMeshNode::checkObstructed() {
 	this->obstructed = false;
 }
 
-void NavMeshNode::addEdge(NavMeshNode* neighbour, int cost) {
+void NavMeshNode::AddEdge(NavMeshNode* neighbour, int cost) {
 	Edge e;
 	e.neighbour = neighbour;
 	e.cost = cost;
 	edges.push_back(e);
 }
 
-bool NavMeshNode::detectImpassableEdge(NavMeshNode* neighbour) {
+bool NavMeshNode::DetectImpassableEdge(NavMeshNode* neighbour) {
 	float diffY = (neighbour->position.y - this->position.y);
 	float diffX = (neighbour->position.x - this->position.x);
 	float diffZ = (neighbour->position.z - this->position.z);
 	return (diffY > diffX || diffY > diffZ);
 }
 
-void NavMeshNode::calculateEdges() {
+void NavMeshNode::CalculateEdges() {
 	//logic to determine the cost of a node to each neighbour
 	for (int node = 0; node < std::size(edges); ++node) {
-		if (detectImpassableEdge(edges[node].neighbour)) {
+		if (DetectImpassableEdge(edges[node].neighbour)) {
 			edges[node].cost = 1000000;
 		}
 		//logic for special terrain types to go here to adjust cost, e.g lava, ice, etc.
