eb333d03c142f65ed42f5665d185e097474495a3
kyledarrenanderson <91319534+kyledarrenanderson@users.noreply.github.com>
2025-03-11 13:57:22 +0000

    Further Setup

--- AdvancedGameTechnologies/build/CSC8503/DemoShootableEnemy.h
+++ AdvancedGameTechnologies/build/CSC8503/DemoShootableEnemy.h
@@ -1,21 +1,32 @@
 #pragma once
 #include "Vector.h"
+#include "GameObject.h"
 
 namespace NCL {
 	namespace CSC8503 {
 		class DemoShootableEnemy {
 		public:
-			DemoShootableEnemy();
-			~DemoShootableEnemy();
+			DemoShootableEnemy(NCL::Maths::Vector3 position, float initialHealth) {
+				this->respawnTimer = 15.0f;
+				Spawn(position, initialHealth);
+			}
+			~DemoShootableEnemy() {
+				this->health = NULL;
+				this->respawnTimer = NULL;
+				this->alive = NULL;
+			}
 			void UpdateRespawnTimer(float dt) { respawnTimer -= dt; }
-			bool CheckRespawn();
-			void Spawn();
+			bool CheckRespawn() { return respawnTimer <= 0; }
+			void Spawn(NCL::Maths::Vector3 position, float initialHealth);
 			void KillEnemy();
-			void UpdateHealth();
+			void UpdateHealth(float damage) { this->health -= damage; }
 			void RegisterHit();
+
+			float GetCurrentHealth() { return this->health; }
+			NCL::Maths::Vector3 GetCurrentPosition();
 		protected:
 			float health;
-			NCL::Maths::Vector3 position;
+			GameObject* enemy;
 			float respawnTimer;
 			bool alive;
 		};
