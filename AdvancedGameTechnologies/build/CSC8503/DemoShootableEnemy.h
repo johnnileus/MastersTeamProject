84a1e80f31c71794f8046f818cb773f7cb1f79be
kyledarrenanderson <91319534+kyledarrenanderson@users.noreply.github.com>
2025-03-11 14:26:45 +0000

    Initialise enemy function

--- AdvancedGameTechnologies/build/CSC8503/DemoShootableEnemy.h
+++ AdvancedGameTechnologies/build/CSC8503/DemoShootableEnemy.h
@@ -6,24 +6,26 @@ namespace NCL {
 	namespace CSC8503 {
 		class DemoShootableEnemy {
 		public:
-			DemoShootableEnemy(NCL::Maths::Vector3 position, float initialHealth) {
+			DemoShootableEnemy() {
+				this->enemy = InitialiseEnemy();
 				this->respawnTimer = 15.0f;
-				Spawn(position, initialHealth);
+				Spawn();
 			}
 			~DemoShootableEnemy() {
 				this->health = NULL;
 				this->respawnTimer = NULL;
 				this->alive = NULL;
 			}
+			GameObject* InitialiseEnemy(float scale, float inverseMass);
 			void UpdateRespawnTimer(float dt) { respawnTimer -= dt; }
 			bool CheckRespawn() { return respawnTimer <= 0; }
-			void Spawn(NCL::Maths::Vector3 position, float initialHealth);
+			void Spawn();
 			void KillEnemy();
 			void UpdateHealth(float damage) { this->health -= damage; }
 			void RegisterHit();
 
 			float GetCurrentHealth() { return this->health; }
-			NCL::Maths::Vector3 GetCurrentPosition();
+			NCL::Maths::Vector3 GetCurrentPosition() { return this->enemy->GetTransform().GetPosition(); }
 		protected:
 			float health;
 			GameObject* enemy;
