556297395bd6be50b47555946f6adac71385c844
kyledarrenanderson <91319534+kyledarrenanderson@users.noreply.github.com>
2025-03-11 14:53:39 +0000

    Fixed file location for CMake

--- AdvancedGameTechnologies/build/CSC8503/DemoShootableEnemy.cpp
+++
@@ -1,9 +0,0 @@
-#include "DemoShootableEnemy.h"
-
-using namespace NCL;
-using namespace CSC8503;
-
-GameObject* DemoShootableEnemy::InitialiseEnemy(float scale, float inverseMass) {
-	GameObject* e = new GameObject();
-
-}
