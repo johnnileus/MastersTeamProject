9199c31a4fccfb028cb09114f8b3f3079482ae32
kyledarrenanderson <91319534+kyledarrenanderson@users.noreply.github.com>
2025-03-12 06:24:11 +0000

    Finished initialise enemy function

--- AdvancedGameTechnologies/CSC8503/DemoShootableEnemy.cpp
+++ AdvancedGameTechnologies/CSC8503/DemoShootableEnemy.cpp
@@ -1,4 +1,5 @@
 #include "DemoShootableEnemy.h"
+#include "TutorialGame.h"
 
 using namespace NCL;
 using namespace CSC8503;
@@ -12,4 +13,12 @@ GameObject* DemoShootableEnemy::InitialiseEnemy(float scale, float inverseMass, 
 	e->GetTransform()
 		.SetScale(Vector3(scale, scale, scale))
 		.SetPosition(position);
+
+	e->SetRenderObject(new RenderObject(&e->GetTransform(), AssetManager::Instance().sphereMesh, AssetManager::Instance().metalTex, AssetManager::Instance().basicShader));
+	e->SetPhysicsObject(new PhysicsObject(&e->GetTransform(), e->GetBoundingVolume()));
+	e->GetPhysicsObject()->SetInverseMass(inverseMass);
+	e->GetPhysicsObject()->InitSphereInertia();
+
+	return e;
+}
 }
