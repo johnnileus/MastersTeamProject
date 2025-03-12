91602f6c90d63285427b17afa620180de8f87ecd
kyledarrenanderson <91319534+kyledarrenanderson@users.noreply.github.com>
2025-03-12 12:39:06 +0000

    updated to include gameworld object

--- AdvancedGameTechnologies/CSC8503/SceneManager.cpp
+++ AdvancedGameTechnologies/CSC8503/SceneManager.cpp
@@ -116,7 +116,6 @@ void SceneManager::AddBullet(Bullet* bullet)
 }
 
 DemoShootableEnemy* SceneManager::AddEnemyToWorld(GameWorld* world, const Vector3& pos, const float scale, float inverseMass) {
-    DemoShootableEnemy* e = new DemoShootableEnemy(scale, inverseMass, pos);
-    world->AddGameObject(e->GetGameObject());
+    DemoShootableEnemy* e = new DemoShootableEnemy(world, scale, inverseMass, pos);
     return e;
 }
