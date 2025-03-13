2236d9c3aa4ebdeb583c206202f5981789a5a68f
gvidas <danieliusgvidas@gmail.com>
2025-03-13 09:05:25 +0000

    moved object gen functions from SceneManager to Scene

--- AdvancedGameTechnologies/CSC8503/SceneManager.h
+++ AdvancedGameTechnologies/CSC8503/SceneManager.h
@@ -8,25 +8,36 @@
 #include "Vector.h"
 #include "Quaternion.h"
 #include "Scene.h"
+#include "Rope.h"
+
 
 namespace NCL {
     namespace CSC8503 {
 
+
         class SceneManager {
         public:
+            SceneManager() {}
+            SceneManager(const SceneManager&) = delete;
+
+            
             // Singleton access
             static SceneManager& Instance();
 
             GameObject* AddCubeToWorld(GameWorld* world, const Vector3& pos, const Vector3& size, float inverseMass);
             GameObject* AddDefaultFloorToWorld(GameWorld* world, const Vector3& position, const Vector3& size);
             GameObject* AddTerrain(GameWorld* world, const Vector3& pos, const Vector3& size);
+            void CreateRopeGroup(GameWorld* world);
+            void GenerateWall(GameWorld* world);
+
+
 
             void UpdateBullets(GameWorld*world, float dt);
             void AddBullet(Bullet* bullet);
 
+            Scene* activeScene;
+
         private:
-            SceneManager() {}
-            SceneManager(const SceneManager&) = delete;
 
             std::vector<Bullet*> bullets;//store all bullets
         };
