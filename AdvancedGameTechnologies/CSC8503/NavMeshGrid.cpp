3f705d9f4afd845a7ab4351767dc0de769d1bbb6
kyledarrenanderson <91319534+kyledarrenanderson@users.noreply.github.com>
2025-03-25 21:34:20 +0000

    Made level smaller, spreadout of nodes

--- AdvancedGameTechnologies/CSC8503/NavMeshGrid.cpp
+++ AdvancedGameTechnologies/CSC8503/NavMeshGrid.cpp
@@ -12,7 +12,7 @@ std::vector<std::vector<NavMeshNode*>> NavMeshGrid::GenerateNavMeshGrid(int leve
 	for (int x = 0; x < levelSize; ++x) {
 		nodeRow.clear();
 		for (int z = 0; z < levelSize; ++z) {
-			NCL::Maths::Vector3 nodePosition = NCL::Maths::Vector3(x - levelSize/2, 10, z - levelSize/2);//replace 500 with the height of the vertex for the node
+			NCL::Maths::Vector3 nodePosition = NCL::Maths::Vector3(2* (x - levelSize/2), 10, 2*(z - levelSize/2));//replace 500 with the height of the vertex for the node
 			nodeRow.emplace_back(new NavMeshNode(nodePosition));
 		}
 		nodeGrid.emplace_back(nodeRow);
