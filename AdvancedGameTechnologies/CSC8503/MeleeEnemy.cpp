725dbb17f738455bd41bf001c6983cdf1b3cbb17
kyledarrenanderson <91319534+kyledarrenanderson@users.noreply.github.com>
2025-03-27 01:34:23 +0000

    rest state

--- AdvancedGameTechnologies/CSC8503/MeleeEnemy.cpp
+++ AdvancedGameTechnologies/CSC8503/MeleeEnemy.cpp
@@ -1,4 +1,5 @@
 #include "MeleeEnemy.h"void MeleeEnemy::RestState(float dt) {
+void MeleeEnemy::RestState(float dt) {
     this->GetPhysicsObject()->SetLinearVelocity(Vector3(0, 0, 0));
     float healthIncrease = maxHealth * 0.01f * dt;
 }
