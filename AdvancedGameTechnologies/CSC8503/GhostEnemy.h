0fbcffee00c5b3ca39313fa3ddf017c99967268f
kyledarrenanderson <91319534+kyledarrenanderson@users.noreply.github.com>
2025-03-27 10:23:30 +0000

    Copied MeleeEnemy to ranged and ghost to get initial testing done

--- AdvancedGameTechnologies/CSC8503/GhostEnemy.h
+++ AdvancedGameTechnologies/CSC8503/GhostEnemy.h
@@ -2,22 +2,51 @@
 #include "NavMeshAgent.h"
 #include "StateMachine.h"
 #include "Player.h"
+#include <vector>
+#include "GameWorld.h"
 
 namespace NCL {
 	namespace CSC8503 {
-		class GhostEnemy : NavMeshAgent {
+		class GhostEnemy : public NavMeshAgent {
 		public:
-			GhostEnemy();
+			GhostEnemy(NavMeshGrid* nodeGrid = nullptr, float scale = 1.0f, float inverseMass = 1.0f, NCL::Maths::Vector3 spawnPostion = NCL::Maths::Vector3(0, 0, 0), float health = 100, bool alive = true, float respawnTimer = 15.0f, float damage = 10.0f, GameWorld* world = nullptr) {
+				this->InitialiseDemoShootableEnemy(scale, inverseMass, spawnPosition, health, alive, respawnTimer, damage);
+				this->InitialiseNavMeshAgent(nodeGrid);
+				InitialiseGhostEnemy(world);
+			}
 			void InitStateMachine();
 			void PatrolState();
 			void ChaseState();
-			void AttackState();
+			void AttackState(float dt);
 			void RetreatState();
-			void GetPlayerNode();
-			void UpdateEnemy();
+			void RestState(float dt);
+			void UpdateEnemy(float dt);
+			void InitialiseGhostEnemy(GameWorld* world) {
+
+				this->stateMachine = new StateMachine();
+				this->currentTarget = nullptr;
+				this->attackCooldown = 3;
+				this->chargeForce = 5;
+
+				auto findPlayers = [this](GameObject* obj) {
+					if (obj->tag == "Player") {
+						Player* player = dynamic_cast<Player*>(obj);
+						if (player) {
+							this->players.push_back(player);
+						}
+					}
+					};
+
+				world->OperateOnContents(findPlayers);
+
+				this->InitStateMachine();
+			}
 		protected:
 			StateMachine* stateMachine;
 			Player* currentTarget;
+			float attackCooldown;
+			float chargeForce;
+			std::vector<Player*> players;
 		};
 	}
 }
--- AdvancedGameTechnologies/CSC8503/RangedEnemy.cpp
+++ AdvancedGameTechnologies/CSC8503/RangedEnemy.cpp
@@ -1,1 +1,165 @@
 #include "RangedEnemy.h"
+#include "Vector.h"
+#include "PhysicsObject.h"
+#include "State.h"
+#include "StateTransition.h"
+
+using namespace NCL;
+using namespace CSC8503;
+
+void  RangedEnemy::InitStateMachine() {
+    State* patrolState = new State([&](float dt) -> void {
+        this->PatrolState();
+        });
+
+    State* chaseState = new State([&](float dt) -> void {
+        this->ChaseState();
+        });
+
+    State* attackState = new State([&](float dt) {
+        this->AttackState(dt);
+        });
+
+    State* retreatState = new State([&](float dt) -> void {
+        this->RetreatState();
+        });
+
+    State* restState = new State([&](float dt) {
+        this->RestState(dt);
+        });
+
+    stateMachine->AddState(patrolState);
+    stateMachine->AddState(chaseState);
+    stateMachine->AddState(attackState);
+    stateMachine->AddState(retreatState);
+    stateMachine->AddState(restState);
+
+    stateMachine->AddTransition(new StateTransition(patrolState, chaseState, [&]() -> bool {
+        return currentTarget && (Vector::Length(this->GetTransform().GetPosition() - currentTarget->GetTransform().GetPosition())) < 20.0f;
+        }));
+
+    stateMachine->AddTransition(new StateTransition(chaseState, patrolState, [&]() -> bool {
+        return !currentTarget || (Vector::Length(this->GetTransform().GetPosition() - currentTarget->GetTransform().GetPosition())) >= 20.0f;
+        }));
+
+    stateMachine->AddTransition(new StateTransition(chaseState, attackState, [&]() -> bool {
+        return currentTarget && (Vector::Length(this->GetTransform().GetPosition() - currentTarget->GetTransform().GetPosition())) < 5.0f;
+        }));
+
+    stateMachine->AddTransition(new StateTransition(attackState, chaseState, [&]() -> bool {
+        return currentTarget && (Vector::Length(this->GetTransform().GetPosition() - currentTarget->GetTransform().GetPosition())) >= 5.0f;
+        }));
+
+    stateMachine->AddTransition(new StateTransition(patrolState, retreatState, [&]() -> bool {
+        return this->currentHealth < 0.15f * this->maxHealth;
+        }));
+
+    stateMachine->AddTransition(new StateTransition(chaseState, retreatState, [&]() -> bool {
+        return this->currentHealth < 0.15f * this->maxHealth;
+        }));
+
+    stateMachine->AddTransition(new StateTransition(attackState, retreatState, [&]() -> bool {
+        return this->currentHealth < 0.15f * this->maxHealth;
+        }));
+
+    stateMachine->AddTransition(new StateTransition(retreatState, restState, [&]() -> bool {
+        return this->currentHealth < 0.15f * this->maxHealth && (Vector::Length(this->GetTransform().GetPosition() - currentTarget->GetTransform().GetPosition())) > 50.0f;
+        }));
+
+    stateMachine->AddTransition(new StateTransition(restState, patrolState, [&]() -> bool {
+        return this->currentHealth > 0.5f * this->maxHealth;
+        }));
+
+    stateMachine->AddTransition(new StateTransition(restState, retreatState, [&]() -> bool {
+        return currentTarget && (Vector::Length(this->GetTransform().GetPosition() - currentTarget->GetTransform().GetPosition())) < 30.0f;
+        }));
+}
+void RangedEnemy::PatrolState() {
+    float closestDistance = std::numeric_limits<float>::max();
+    NCL::Maths::Vector3 enemyPosition = this->GetTransform().GetPosition();
+
+    for (Player* player : players) {
+        float distance = Vector::Length(enemyPosition - player->GetTransform().GetPosition());
+        if (distance < closestDistance) {
+            closestDistance = distance;
+            this->currentTarget = player;
+        }
+    }
+
+
+    if (this->currentNode == this->destination || this->destination == nullptr || this->path.size() == 0) {
+        SetDestination();
+        setCurrentNode(this->GetCurrentPosition().x, this->GetCurrentPosition().z);
+        FindPath();
+    }
+    FollowPath();
+}
+
+void RangedEnemy::ChaseState() {
+    if (currentTarget) {
+        NCL::Maths::Vector3 playerPosition = currentTarget->GetTransform().GetPosition();
+
+        NavMeshNode* closestNode = nullptr;
+        float closestDistance = std::numeric_limits<float>::max();
+        for (NavMeshNode* node : nodeGrid->GetAllNodes()) {
+            float distance = Vector::Length(playerPosition - node->GetPosition());
+            if (distance < closestDistance) {
+                closestDistance = distance;
+                closestNode = node;
+            }
+        }
+
+        if (closestNode && closestNode != this->destination || this->path.size() == 0) {
+            this->destination = closestNode;
+            FindPath();
+        }
+        if (this->destination != nullptr) {
+            FollowPath();
+        }
+    }
+}
+
+void RangedEnemy::AttackState(float dt) {
+    if (attackCooldown <= 0) {
+        NCL::Maths::Vector3 direction = Vector::Normalise(currentTarget->GetTransform().GetPosition() - this->GetTransform().GetPosition());
+        this->GetPhysicsObject()->AddForce(direction * chargeForce);
+        attackCooldown = 3;
+    }
+    else {
+        attackCooldown -= dt;
+    }
+}
+
+void RangedEnemy::RetreatState() {
+    NCL::Maths::Vector3 playerPosition = currentTarget->GetTransform().GetPosition();
+    NCL::Maths::Vector3 enemyPosition = this->GetTransform().GetPosition();
+    NCL::Maths::Vector3 direction = Vector::Normalise(enemyPosition - playerPosition);
+    NCL::Maths::Vector3 retreatPosition = enemyPosition + (direction * 50.0f);
+
+    NavMeshNode* retreatNode = nullptr;
+    float closestDistance = std::numeric_limits<float>::max();
+    for (NavMeshNode* node : nodeGrid->GetAllNodes()) {
+        float distance = Vector::Length(node->GetPosition() - retreatPosition);
+        if (distance < closestDistance) {
+            closestDistance = distance;
+            retreatNode = node;
+        }
+    }
+
+    if (retreatNode && retreatNode != this->destination) {
+        this->destination = retreatNode;
+        FindPath();
+    }
+    if (this->destination != nullptr) {
+        FollowPath();
+    }
+}
+
+void RangedEnemy::RestState(float dt) {
+    this->GetPhysicsObject()->SetLinearVelocity(Vector3(0, 0, 0));
+    float healthIncrease = maxHealth * 0.01f * dt;
+}
+
+void RangedEnemy::UpdateEnemy(float dt) {
+    this->stateMachine->Update(dt);
+}
