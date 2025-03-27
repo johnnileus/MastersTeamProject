876a3d34fcf57ad53d4d7ee5f32f900188e983b4
Bow Silva <c1030300@newcastle.ac.uk>
2025-03-27 18:30:00 +0000

    Changed passive item init world type

--- AdvancedGameTechnologies/CSC8503/TutorialGame.cpp
+++ AdvancedGameTechnologies/CSC8503/TutorialGame.cpp
@@ -463,9 +463,6 @@ void TutorialGame::ReloadLevel() {
 	std::cout << "Level reloaded!" << std::endl;
 }
 
-void TutorialGame::Transition() {
-	return;
-}
 
 
 void TutorialGame::InitNavigationTestLevel() {
@@ -502,7 +499,7 @@ void TutorialGame::InitItems() {
 	for (int i = 0; i < 2; i++) {
 		int x = 10;
 		int rand = (std::rand() % 5) + 1;
-		PassiveItem::Instantiate(world, itemList, player, Vector3(x + (i * 10), 0, 20), rand);
+		PassiveItem::Instantiate(&world, itemList, player, Vector3(x + (i * 10), 0, 20), rand);
 	}
 }
 
