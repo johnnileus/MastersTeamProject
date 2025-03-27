95966760460a74b1192f7f5aff8f349ce1c56adc
kyledarrenanderson <91319534+kyledarrenanderson@users.noreply.github.com>
2025-03-27 17:20:55 +0000

    Updated allnodes to only store none obstructed nodes

--- AdvancedGameTechnologies/CSC8503/NavMeshGrid.h
+++ AdvancedGameTechnologies/CSC8503/NavMeshGrid.h
@@ -1,18 +1,23 @@
 #pragma once
 #include "NavMeshNode.h"
 #include <vector>
+#include "GameWorld.h"
 
 namespace NCL {
 	namespace CSC8503 {
 		class NavMeshNode;
 		class NavMeshGrid {
 		public:
-			NavMeshGrid() { 
+			NavMeshGrid(GameWorld* world) { 
 				levelSize = 256; 
+				this->world = world;
 				nodeGrid = GenerateNavMeshGrid(levelSize);
 				for (int x = 0; x < levelSize; ++x) {
 					for (int z = 0; z < levelSize; ++z) {
-						allNodes.emplace_back(nodeGrid[x][z]);
+						if (!nodeGrid[x][z]->isObstructed()) {
+							allNodes.emplace_back(nodeGrid[x][z]);
+						}
+						
 					}
 				}
 			}
@@ -24,6 +29,7 @@ namespace NCL {
 			std::vector<std::vector<NavMeshNode*>>nodeGrid;
 			std::vector<std::vector<NavMeshNode*>>GenerateNavMeshGrid(int levelSize);
 			std::vector<NavMeshNode*>allNodes;
+			GameWorld* world;
 		};
 	}
 }
