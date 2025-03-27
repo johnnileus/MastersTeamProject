2b96b7b6a46f5d4c242b21bb35475626cf589d4a
kyledarrenanderson <91319534+kyledarrenanderson@users.noreply.github.com>
2025-03-27 01:33:34 +0000

    replaced costs to fix floating point error

--- AdvancedGameTechnologies/CSC8503/NavMeshGrid.cpp
+++ AdvancedGameTechnologies/CSC8503/NavMeshGrid.cpp
@@ -21,59 +21,59 @@ std::vector<std::vector<NavMeshNode*>> NavMeshGrid::GenerateNavMeshGrid(int leve
 		for (int z = 0; z < nodeGrid.size(); ++z) {
 			nodeGrid[x][z]->checkObstructed();
             if (x - 1 < 0 && z - 1 < 0) {//top left corner
-                nodeGrid[x][z]->AddEdge(nodeGrid[x][z + 1], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x][z + 1]->GetPosition()));
-                nodeGrid[x][z]->AddEdge(nodeGrid[x + 1][z], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x + 1][z]->GetPosition()));
-                nodeGrid[x][z]->AddEdge(nodeGrid[x + 1][z + 1], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x + 1][z + 1]->GetPosition()));
+                nodeGrid[x][z]->AddEdge(nodeGrid[x][z + 1], 1);
+                nodeGrid[x][z]->AddEdge(nodeGrid[x + 1][z], 1);
+                nodeGrid[x][z]->AddEdge(nodeGrid[x + 1][z + 1], 1);
             }
             else if (x - 1 < 0 && z + 1 >= nodeGrid.size()) {//top right corner
-                nodeGrid[x][z]->AddEdge(nodeGrid[x][z - 1], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x][z - 1]->GetPosition()));
-                nodeGrid[x][z]->AddEdge(nodeGrid[x + 1][z], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x + 1][z]->GetPosition()));
-                nodeGrid[x][z]->AddEdge(nodeGrid[x + 1][z - 1], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x + 1][z - 1]->GetPosition()));
+                nodeGrid[x][z]->AddEdge(nodeGrid[x][z - 1], 1);
+                nodeGrid[x][z]->AddEdge(nodeGrid[x + 1][z], 1);
+                nodeGrid[x][z]->AddEdge(nodeGrid[x + 1][z - 1], 1);
             }
             else if (x + 1 >= nodeGrid.size() && z - 1 < 0) {//bottom left corner
-                nodeGrid[x][z]->AddEdge(nodeGrid[x][z + 1], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x][z + 1]->GetPosition()));
-                nodeGrid[x][z]->AddEdge(nodeGrid[x - 1][z], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x - 1][z]->GetPosition()));
-                nodeGrid[x][z]->AddEdge(nodeGrid[x - 1][z + 1], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x - 1][z + 1]->GetPosition()));
+                nodeGrid[x][z]->AddEdge(nodeGrid[x][z + 1], 1);
+                nodeGrid[x][z]->AddEdge(nodeGrid[x - 1][z], 1);
+                nodeGrid[x][z]->AddEdge(nodeGrid[x - 1][z + 1], 1);
             }
             else if (x + 1 >= nodeGrid.size() && z + 1 >= nodeGrid.size()) {//bottom right corner
-                nodeGrid[x][z]->AddEdge(nodeGrid[x][z - 1], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x][z - 1]->GetPosition()));
-                nodeGrid[x][z]->AddEdge(nodeGrid[x - 1][z], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x - 1][z]->GetPosition()));
-                nodeGrid[x][z]->AddEdge(nodeGrid[x - 1][z - 1], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x - 1][z - 1]->GetPosition()));
+                nodeGrid[x][z]->AddEdge(nodeGrid[x][z - 1], 1);
+                nodeGrid[x][z]->AddEdge(nodeGrid[x - 1][z], 1);
+                nodeGrid[x][z]->AddEdge(nodeGrid[x - 1][z - 1], 1);
             }
             else if (z - 1 < 0) {//left edge
-                nodeGrid[x][z]->AddEdge(nodeGrid[x - 1][z], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x - 1][z]->GetPosition()));
-                nodeGrid[x][z]->AddEdge(nodeGrid[x + 1][z], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x + 1][z]->GetPosition()));
-                nodeGrid[x][z]->AddEdge(nodeGrid[x][z + 1], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x][z + 1]->GetPosition()));
-                nodeGrid[x][z]->AddEdge(nodeGrid[x - 1][z + 1], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x - 1][z + 1]->GetPosition()));
-                nodeGrid[x][z]->AddEdge(nodeGrid[x + 1][z + 1], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x + 1][z + 1]->GetPosition()));
+                nodeGrid[x][z]->AddEdge(nodeGrid[x - 1][z], 1);
+                nodeGrid[x][z]->AddEdge(nodeGrid[x + 1][z], 1);
+                nodeGrid[x][z]->AddEdge(nodeGrid[x][z + 1], 1);
+                nodeGrid[x][z]->AddEdge(nodeGrid[x - 1][z + 1], 1);
+                nodeGrid[x][z]->AddEdge(nodeGrid[x + 1][z + 1], 1);
             }
             else if (z + 1 >= nodeGrid.size()) {//right edge
-                nodeGrid[x][z]->AddEdge(nodeGrid[x - 1][z], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x - 1][z]->GetPosition()));
-                nodeGrid[x][z]->AddEdge(nodeGrid[x + 1][z], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x + 1][z]->GetPosition()));
-                nodeGrid[x][z]->AddEdge(nodeGrid[x][z - 1], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x][z - 1]->GetPosition()));
-                nodeGrid[x][z]->AddEdge(nodeGrid[x - 1][z - 1], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x - 1][z - 1]->GetPosition()));
-                nodeGrid[x][z]->AddEdge(nodeGrid[x + 1][z - 1], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x + 1][z - 1]->GetPosition()));
+                nodeGrid[x][z]->AddEdge(nodeGrid[x - 1][z], 1);
+                nodeGrid[x][z]->AddEdge(nodeGrid[x + 1][z], 1);
+                nodeGrid[x][z]->AddEdge(nodeGrid[x][z - 1], 1);
+                nodeGrid[x][z]->AddEdge(nodeGrid[x - 1][z - 1], 1);
+                nodeGrid[x][z]->AddEdge(nodeGrid[x + 1][z - 1],1);
             }
             else if (x - 1 < 0) {//top edge
-                nodeGrid[x][z]->AddEdge(nodeGrid[x][z - 1], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x][z - 1]->GetPosition()));
-                nodeGrid[x][z]->AddEdge(nodeGrid[x][z + 1], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x][z + 1]->GetPosition()));
-                nodeGrid[x][z]->AddEdge(nodeGrid[x + 1][z - 1], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x + 1][z - 1]->GetPosition()));
-                nodeGrid[x][z]->AddEdge(nodeGrid[x + 1][z], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x + 1][z]->GetPosition()));
-                nodeGrid[x][z]->AddEdge(nodeGrid[x + 1][z + 1], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x + 1][z + 1]->GetPosition()));
+                nodeGrid[x][z]->AddEdge(nodeGrid[x][z - 1], 1);
+                nodeGrid[x][z]->AddEdge(nodeGrid[x][z + 1], 1);
+                nodeGrid[x][z]->AddEdge(nodeGrid[x + 1][z - 1], 1);
+                nodeGrid[x][z]->AddEdge(nodeGrid[x + 1][z], 1);
+                nodeGrid[x][z]->AddEdge(nodeGrid[x + 1][z + 1], 1);
             }
             else if (x + 1 >= nodeGrid.size()) {//bottom edge
-                nodeGrid[x][z]->AddEdge(nodeGrid[x][z - 1], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x][z - 1]->GetPosition()));
-                nodeGrid[x][z]->AddEdge(nodeGrid[x][z + 1], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x][z + 1]->GetPosition()));
-                nodeGrid[x][z]->AddEdge(nodeGrid[x - 1][z - 1], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x - 1][z - 1]->GetPosition()));
-                nodeGrid[x][z]->AddEdge(nodeGrid[x - 1][z], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x - 1][z]->GetPosition()));
-                nodeGrid[x][z]->AddEdge(nodeGrid[x - 1][z + 1], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x - 1][z + 1]->GetPosition()));
+                nodeGrid[x][z]->AddEdge(nodeGrid[x][z - 1], 1);
+                nodeGrid[x][z]->AddEdge(nodeGrid[x][z + 1], 1);
+                nodeGrid[x][z]->AddEdge(nodeGrid[x - 1][z - 1], 1);
+                nodeGrid[x][z]->AddEdge(nodeGrid[x - 1][z], 1);
+                nodeGrid[x][z]->AddEdge(nodeGrid[x - 1][z + 1], 1);
             }
             else {//somewhere in the middle
                 for (int row = -1; row <= 1; ++row) {
                     for (int col = -1; col <= 1; ++col) {
                         if (row == 0 && col == 0) continue;//ignore self
                         if (!nodeGrid[x + row][z + col]->isObstructed()) {
-                            nodeGrid[x][z]->AddEdge(nodeGrid[x + row][z + col], CalculateDistance(nodeGrid[x][z]->GetPosition(), nodeGrid[x + row][z + col]->GetPosition()));
+                            nodeGrid[x][z]->AddEdge(nodeGrid[x + row][z + col], 1);
                         }
                     }
                 }
@@ -83,9 +83,4 @@ std::vector<std::vector<NavMeshNode*>> NavMeshGrid::GenerateNavMeshGrid(int leve
 		}
 	}
 	return nodeGrid;
-}
-
-float  NavMeshGrid::CalculateDistance(const NCL::Maths::Vector3& pos1, const NCL::Maths::Vector3& pos2) {
-    NCL::Maths::Vector3 difference = pos1 - pos2;
-    return std::abs(std::sqrtf((difference.x * difference.x) + (difference.z * difference.z)));
 }
