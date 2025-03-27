ceb17b150dee9620c041169398bfe6757f13875e
kyledarrenanderson <91319534+kyledarrenanderson@users.noreply.github.com>
2025-03-27 17:21:59 +0000

    Fixed the follow path system to be smoother and work with any enemy size

--- AdvancedGameTechnologies/CSC8503/NavMeshAgent.cpp
+++ AdvancedGameTechnologies/CSC8503/NavMeshAgent.cpp
@@ -121,14 +121,17 @@ void NavMeshAgent::FollowPath() {
         setCurrentNode(roundedX, roundedZ);
     }
 
-    if (this->currentNode == this->nextNode && !this->path.empty()) {
-        this->path.erase(this->path.begin());
-        if (!this->path.empty()) {
-            //this->GetPhysicsObject()->SetLinearVelocity(Vector3(0, 0, 0));
-            this->nextNode = this->path.front();
+    if (this->currentNode && this->nextNode && !this->path.empty()) {
+        float distance = Vector::Length(this->currentNode->GetPosition() - this->nextNode->GetPosition());
+        if (distance <= this->scale) {
+            this->path.erase(this->path.begin());
+            if (!this->path.empty()) {
+                this->nextNode = this->path.front();
+            }
         }
     }
-    else if (!this->path.empty()) {
+
+    if (!this->path.empty()) {
         this->nextNode = this->path.front();
         for (int n = 0; n < size(path) - 1; ++n) {
             Debug::DrawLine(path[n]->GetPosition(), path[n + 1]->GetPosition(), Vector4(1, 0, 0, 0), 0.1f);
@@ -138,8 +141,6 @@ void NavMeshAgent::FollowPath() {
     MoveTowardsNextNode();
 }
 
-//agent slides around and misses nodes?
-//could also be due to the errors in the pathfinding route
 void NavMeshAgent::MoveTowardsNextNode() {
     if (this->nextNode == nullptr) {
         return;
@@ -147,7 +148,20 @@ void NavMeshAgent::MoveTowardsNextNode() {
 
     const auto& currPos = this->GetCurrentPosition();
     const auto& nextPos = this->nextNode->GetPosition();
-    this->GetPhysicsObject()->AddForce(Vector::Normalise(Vector3((nextPos.x - currPos.x), 0, (nextPos.z - currPos.z))) * Vector3(50,50,50));
+    Vector3 direction = Vector::Normalise(Vector3((nextPos.x - currPos.x), 0, (nextPos.z - currPos.z)));
+    float lerpFactor = 2.5f;
+    Vector3 currentDirection = Vector::Normalise(this->GetPhysicsObject()->GetLinearVelocity());
+    Vector3 smoothDirection = Vector::Lerp(currentDirection, direction, lerpFactor);
+
+    this->GetPhysicsObject()->AddForce(smoothDirection * Vector3(50, 50, 50));
+
+    float maxSpeed = 10.0f;
+    Vector3 velocity = this->GetPhysicsObject()->GetLinearVelocity();
+    float speed = Vector::Length(velocity);
+    if (speed > maxSpeed) {
+        Vector3 clampedVelocity = Vector::Normalise(velocity) * maxSpeed;
+        this->GetPhysicsObject()->SetLinearVelocity(clampedVelocity);
+    }
 }
 
 void NavMeshAgent::SetDestination() {
@@ -160,6 +174,7 @@ void NavMeshAgent::SetDestinationNull() {
 }
 
 void NavMeshAgent::Spawn() {
+    this->GetPhysicsObject()->SetLinearVelocity(Vector3(0, 0, 0));
     this->spawnPosition = this->nodeGrid->GetAllNodes()[(rand() + rand() + rand()) % this->nodeGrid->GetAllNodes().size()]->GetPosition();
     this->currentHealth = this->maxHealth;
     this->alive = true;
